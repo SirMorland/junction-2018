748d1e7ab71891a0721ed5243a9b306a1a61f34c
Vilma Lappi <wiiwiih@hotmail.com>
2018-11-24 14:24:35 +0200

    Korttien valinta
    Valitun kortin erottaa visuaalisesti, kortin voi valita

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -1,22 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class SelectionManager : MonoBehaviour {
 
     [SerializeField] private GameObject leftCard;
     [SerializeField] private GameObject rightCard;
+    [SerializeField] private EventSystem eventSys;
+    private float direction = 0f;
+
+    public int player;
 
     // Use this for initialization
     void Start () {
-		
-	}
+        eventSys.SetSelectedGameObject(leftCard);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("error"))
-        {
+        direction = Input.GetAxis("Horizontal-" + player);
 
+        if (Input.GetButtonDown("SelectCard-" + player))
+        {
+            eventSys.currentSelectedGameObject.GetComponentInChildren<Text>().text = "Selected";
         }
-	}
+
+        if (direction < 0) eventSys.SetSelectedGameObject(leftCard);
+        else if (direction > 0) eventSys.SetSelectedGameObject(rightCard);
+
+        
+    }
 }
