fe17a47257398e38a6a84da75b7be13fe5d28bbe
Vilma Lappi <wiiwiih@hotmail.com>
2019-02-11 23:48:37 +0200

    Tilastojen toteutus aloitettu + korjaus kortin valintaan

---
+++ Assets/Scripts/Stats/StatSaver.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+//Used the following Unity tutorial as base: https://unity3d.com/learn/tutorials/topics/scripting/introduction-saving-and-loading
+public class StatSaver : MonoBehaviour {
+
+    public Stats statData;
+    const string folderName = "Stats";
+    const string fileName = "stats.dat";
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Stats");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Save()
+    {
+        string folderPath = Path.Combine(Application.persistentDataPath, folderName);
+        if (!Directory.Exists(folderPath))
+            Directory.CreateDirectory(folderPath);
+
+        string dataPath = Path.Combine(folderPath, fileName);
+        SaveStats(statData, dataPath);
+    }
+
+    void Load()
+    {
+        string[] filePaths = GetFilePaths();
+
+        if (filePaths.Length > 0)
+            statData = LoadStats(filePaths[0]);
+    }
+
+    static void SaveStats(Stats data, string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        using (FileStream fileStream = File.Open(path, FileMode.OpenOrCreate))
+        {
+            binaryFormatter.Serialize(fileStream, data);
+        }
+    }
+
+    static Stats LoadStats(string path)
+    {
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+        using (FileStream fileStream = File.Open(path, FileMode.Open))
+        {
+            return (Stats)binaryFormatter.Deserialize(fileStream);
+        }
+    }
+
+    static string[] GetFilePaths()
+    {
+        string folderPath = Path.Combine(Application.persistentDataPath, folderName);
+
+        return Directory.GetFiles(folderPath, fileName);
+    }
+}
---
+++ Assets/Scripts/Stats/Stats.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+[Serializable]
+
+public class Stats
+{
+    public int gamesPlayed;
+    public int p1Wins;
+    public int p2Wins;
+}
